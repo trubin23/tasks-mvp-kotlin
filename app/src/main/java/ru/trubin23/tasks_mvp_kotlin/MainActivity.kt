d83353d6bace882c1884299971f56fee7db04180
Andrey Trubin <trubin23@gmail.com>
2018-05-10 23:52:06 +0500

    Initial commit

---
+++ app/src/main/java/ru/trubin23/tasks_mvp_kotlin/MainActivity.kt
@@ -0,0 +1,15 @@
+package ru.trubin23.tasks_mvp_kotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Andrey on 10.05.2018.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
