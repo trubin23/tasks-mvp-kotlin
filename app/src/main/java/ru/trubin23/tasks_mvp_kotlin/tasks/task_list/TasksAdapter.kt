29626abedc1b965ff22076816c0b6a9afcab786e
Andrey Trubin <trubin23@gmail.com>
2018-05-20 21:00:34 +0500

    implementation of the TasksAdapter class

--- app/src/main/java/ru/trubin23/tasks_mvp_kotlin/tasks/task_list/TasksAdapter.kt
+++ app/src/main/java/ru/trubin23/tasks_mvp_kotlin/tasks/task_list/TasksAdapter.kt
@@ -1,30 +1,34 @@
 package ru.trubin23.tasks_mvp_kotlin.tasks.task_list
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import ru.trubin23.tasks_mvp_kotlin.R
 import ru.trubin23.tasks_mvp_kotlin.data.Task
+import java.io.File
 import java.util.ArrayList
 
 class TasksAdapter(private val itemListener: TaskItemListener)
     : BaseAdapter() {
 
     var mTasks : List<Task> = ArrayList(0)
+        set(tasks) {
+            field = tasks
+            notifyDataSetChanged()
+        }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val rowView = convertView ?: LayoutInflater.from(parent?.context)
+                .inflate(R.layout.task_item, parent, false)
+
+        return rowView
     }
 
-    override fun getItem(position: Int): Any {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getCount() = mTasks.size
 
-    override fun getItemId(position: Int): Long {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getItem(position: Int) = mTasks[position]
 
-    override fun getCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getItemId(position: Int) = position.toLong()
 
 }
