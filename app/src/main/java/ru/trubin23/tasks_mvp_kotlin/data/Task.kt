8e1bdb63506a50e26e0eb490c62816a93df36492
Andrey Trubin <trubin23@gmail.com>
2018-05-26 12:41:13 +0500

    start implementation TasksDao

--- app/src/main/java/ru/trubin23/tasks_mvp_kotlin/data/Task.kt
+++ app/src/main/java/ru/trubin23/tasks_mvp_kotlin/data/Task.kt
@@ -1,9 +1,16 @@
 package ru.trubin23.tasks_mvp_kotlin.data
 
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
 import java.util.*
 
-class Task(var mTitle: String = "", var mDescription: String = "",
-           var mId: String = UUID.randomUUID().toString()) {
+@Entity(tableName = "tasks")
+data class Task @JvmOverloads constructor(
+        var mTitle: String = "",
+        var mDescription: String = "",
+        @PrimaryKey @ColumnInfo(name = "entryId") var mId: String = UUID.randomUUID().toString()
+) {
 
     var isCompleted = false
 
--- app/src/main/java/ru/trubin23/tasks_mvp_kotlin/data/source/local/TasksDao.kt
+++ app/src/main/java/ru/trubin23/tasks_mvp_kotlin/data/source/local/TasksDao.kt
@@ -1,4 +1,23 @@
 package ru.trubin23.tasks_mvp_kotlin.data.source.local
 
+import android.arch.persistence.room.*
+import ru.trubin23.tasks_mvp_kotlin.data.Task
+
+@Dao
 interface TasksDao {
+
+    @Query("SELECT * FROM tasks")
+    fun getTasks(): List<Task>
+
+    @Query("SELECT * FROM tasks WHERE entryId = :taskId")
+    fun getTaskById(taskId: String): Task?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertTask(task: Task)
+
+    @Update
+    fun updateTask(task: Task): Int
+
+    @Query("UPDATE tasks SET completed = :completed WHERE entryId = :taskId")
+    fun updateCompleted(taskId: String, completed: Boolean)
 }
