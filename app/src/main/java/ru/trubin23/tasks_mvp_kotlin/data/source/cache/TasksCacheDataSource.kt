d36afd15e8b24b5c9a6ff8c2c77eb04cf6d5c46a
Andrey Trubin <trubin23@gmail.com>
2018-06-08 07:43:57 +0500

    start implementation of CacheRepository

--- app/src/main/java/ru/trubin23/tasks_mvp_kotlin/data/source/cache/TasksCacheDataSource.kt
+++ app/src/main/java/ru/trubin23/tasks_mvp_kotlin/data/source/cache/TasksCacheDataSource.kt
@@ -1,4 +1,22 @@
 package ru.trubin23.tasks_mvp_kotlin.data.source.cache
 
+import ru.trubin23.tasks_mvp_kotlin.data.Task
+
 interface TasksCacheDataSource {
+
+    fun getTasks(): List<Task>
+
+    fun setTasks(tasks: List<Task>)
+
+    fun getTaskById(taskId: String): Task
+
+    fun addTask(task: Task)
+
+    fun removeTask(taskId: String)
+
+    fun completedTask(taskId: String, completed: Boolean)
+
+    fun clearCompletedTask()
+
+    fun irrelevantState()
 }
--- app/src/main/java/ru/trubin23/tasks_mvp_kotlin/data/source/cache/TasksCacheRepository.kt
+++ app/src/main/java/ru/trubin23/tasks_mvp_kotlin/data/source/cache/TasksCacheRepository.kt
@@ -1,6 +1,40 @@
 package ru.trubin23.tasks_mvp_kotlin.data.source.cache
 
+import ru.trubin23.tasks_mvp_kotlin.data.Task
+
 class TasksCacheRepository : TasksCacheDataSource {
+
+    override fun getTasks(): List<Task> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setTasks(tasks: List<Task>) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getTaskById(taskId: String): Task {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun addTask(task: Task) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun removeTask(taskId: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun completedTask(taskId: String, completed: Boolean) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun clearCompletedTask() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun irrelevantState() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     companion object {
         private var INSTANCE: TasksCacheRepository? = null
