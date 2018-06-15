88555758ce6402ff1755affe2d13898226d7e11d
Andrey Trubin <trubin23@gmail.com>
2018-06-15 23:44:34 +0500

    start adding AddEditTask package

---
+++ app/src/main/java/ru/trubin23/tasks_mvp_kotlin/addedittask/AddEditTaskActivity.kt
@@ -0,0 +1,13 @@
+package ru.trubin23.tasks_mvp_kotlin.addedittask
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import ru.trubin23.tasks_mvp_kotlin.R
+
+class AddEditTaskActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_edit)
+    }
+}
