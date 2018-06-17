403ca99d9b4d167c6f2948ba8a5c6ac7696416f7
Andrey Trubin <trubin23@gmail.com>
2018-06-17 17:16:51 +0500

    start adding Statistics package

---
+++ app/src/main/java/ru/trubin23/tasks_mvp_kotlin/statistics/StatisticsActivity.kt
@@ -0,0 +1,13 @@
+package ru.trubin23.tasks_mvp_kotlin.statistics
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import ru.trubin23.tasks_mvp_kotlin.R
+
+class StatisticsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_statistics)
+    }
+}
