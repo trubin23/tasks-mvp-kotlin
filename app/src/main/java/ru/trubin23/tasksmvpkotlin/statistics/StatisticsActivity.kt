a841ffd92a7407733709282d1c52701c687791e9
Trubin Andrey <trubin23@gmail.com>
2018-07-22 23:01:48 +0500

    NavigationView added to StatisticsActivity

--- app/src/main/java/ru/trubin23/tasksmvpkotlin/statistics/StatisticsActivity.kt
+++ app/src/main/java/ru/trubin23/tasksmvpkotlin/statistics/StatisticsActivity.kt
@@ -1,6 +1,8 @@
 package ru.trubin23.tasksmvpkotlin.statistics
 
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.AppCompatActivity
 import ru.trubin23.tasksmvpkotlin.R
 import ru.trubin23.tasksmvpkotlin.util.Injection
@@ -8,15 +10,35 @@ import ru.trubin23.tasksmvpkotlin.util.addFragmentToActivity
 
 class StatisticsActivity : AppCompatActivity() {
 
+    private lateinit var mDrawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.statistics_act)
 
+        supportActionBar?.run {
+            setTitle(R.string.statistics)
+            setHomeAsUpIndicator(R.drawable.ic_menu)
+            setDisplayHomeAsUpEnabled(true)
+        }
+
+        mDrawerLayout = findViewById(R.id.drawer_layout)
+
+        setupDrawerContent(findViewById(R.id.nav_view))
+
         val addEditTaskFragment = supportFragmentManager.findFragmentById(R.id.content_frame)
-                as StatisticsFragment ? ?: StatisticsFragment.newInstance().also {
+                as StatisticsFragment? ?: StatisticsFragment.newInstance().also {
             addFragmentToActivity(it, R.id.content_frame)
         }
 
         StatisticsPresenter(Injection.provideTasksRepository(applicationContext), addEditTaskFragment)
+    }
+
+    private fun setupDrawerContent(navigationView: NavigationView) {
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            menuItem.isChecked = true
+            mDrawerLayout.closeDrawers()
+            true
+        }
     }
 }
