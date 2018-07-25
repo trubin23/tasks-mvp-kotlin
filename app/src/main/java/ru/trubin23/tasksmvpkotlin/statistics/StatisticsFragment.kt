809f3e1552a2ba387976c9ebc9de95a3f685f6a4
Trubin Andrey <trubin23@gmail.com>
2018-07-25 23:45:19 +0500

    implemented functions in class the StatisticsFragment

--- app/src/main/java/ru/trubin23/tasksmvpkotlin/statistics/StatisticsFragment.kt
+++ app/src/main/java/ru/trubin23/tasksmvpkotlin/statistics/StatisticsFragment.kt
@@ -5,24 +5,48 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import ru.trubin23.tasksmvpkotlin.R
 
 class StatisticsFragment : Fragment(), StatisticsContract.View {
+
+    private lateinit var mStatisticsTextView: TextView
 
     override lateinit var mPresenter: StatisticsContract.Presenter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.statistics_frag, container, false)
+        val root = inflater.inflate(R.layout.statistics_frag, container, false)
+        mStatisticsTextView = root.findViewById(R.id.statistics)
+        return root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mPresenter.start()
     }
 
     override fun setProgressIndicator(active: Boolean) {
+        if (active){
+            mStatisticsTextView.text = getString(R.string.loading)
+        } else {
+            mStatisticsTextView.text = ""
+        }
     }
 
     override fun showStatistics(numberOfActiveTasks: Int, numberOfCompletedTasks: Int) {
+        if (numberOfActiveTasks==0 && numberOfCompletedTasks==0){
+            mStatisticsTextView.text = getString(R.string.statistics_no_tasks)
+        } else {
+            mStatisticsTextView.text = "${getString(R.string.statistics_active_tasks)} "+
+                    "$numberOfActiveTasks\n"+
+                    "${getString(R.string.statistics_completed_tasks)} "+
+                    "$numberOfCompletedTasks"
+        }
     }
 
     override fun showLoadingStatisticsError() {
+        mStatisticsTextView.text = getString(R.string.statistics_error)
     }
 
     companion object {
